980f51ae6a1ac312e0d459512bc1a02e996f7f30
Камиль Хакимов <105122311+kamil116rus@users.noreply.github.com>
2023-11-06 11:06:06 +0300

    Task 6 add doсument controller and document service (#3)
    * Task-6]-add-dokumentController-documentService
    
    * Task-6]-add-dokumentController-documentService
    
    * Task-6]-add-dokumentController-documentService
    
    * Task-6]-add-dokumentController-documentService
    
    * изменил настройки и переписал сервисы , изменил название
    
    * изменил тесты
    
    * изменил тесты
    
    * изменил тесты
    
    * в файле POM поменя версию мокито на переменную
    
    * Убрал аннотацию
    
    * Убрал kлишние комменты
    
    * Task-6]-add-dokumentController-documentService
    
    Task-6]-add-dokumentController-documentService
    
    Task-6]-add-dokumentController-documentService
    
    Task-6]-add-dokumentController-documentService
    
    изменил настройки и переписал сервисы , изменил название
    
    изменил тесты
    
    изменил тесты
    
    изменил тесты
    
    в файле POM поменя версию мокито на переменную
    
    Убрал аннотацию
    
    Убрал kлишние комменты
    
    * Убрал kлишние комменты
    
    ---------
    
    Co-authored-by: kamil <kh8@ramler.ru>

--- src/main/java/org/kata/service/impl/DocumentServiceImpl.java
+++ src/main/java/org/kata/service/impl/DocumentServiceImpl.java
@@ -1,11 +1,100 @@
 package org.kata.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.kata.config.UrlProperties;
+import org.kata.dto.DocumentDto;
+import org.kata.dto.enums.DocumentType;
+import org.kata.exception.DocumentsNotFoundException;
 import org.kata.service.DocumentService;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+/**
+ * This class is designed to work with individual documents.
+ */
 @Service
 @Slf4j
 public class DocumentServiceImpl implements DocumentService {
+    private final UrlProperties urlProperties;
+    private final WebClient loaderWebClient;
 
+
+    public DocumentServiceImpl(UrlProperties urlProperties) {
+        this.urlProperties = urlProperties;
+        this.loaderWebClient = WebClient.create(urlProperties.getProfileServiceBaseUrl());
+    }
+
+    /**
+     * Method to get a list of all documents by icp.
+     *
+     * @param icp - Individual's icp.
+     * @return List of DocumentDto.
+     */
+    @Override
+    public List<DocumentDto> getAllDocuments(String icp) {
+        return loaderWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(urlProperties.getProfileServiceGetAllDocuments())
+                        .queryParam("icp", icp)
+                        .build())
+                .retrieve()
+                .onStatus(HttpStatus::isError, response ->
+                        Mono.error(new DocumentsNotFoundException(
+                                "Documents with icp " + icp + " not found")
+                        )
+                )
+                .bodyToMono(new ParameterizedTypeReference<List<DocumentDto>>() {
+                })
+                .block();
+    }
+
+
+    /**
+     * Method to get a list of all documents by icp.
+     *
+     * @param icp - Individual's icp.
+     * @return List of DocumentDto.
+     */
+    public List<DocumentDto> getActualDocuments(String icp) {
+        return getAllDocuments(icp).stream().filter(DocumentDto::isActual).toList();
+    }
+
+
+    /**
+     * Method to get a list of archive documents by icp.
+     *
+     * @param icp - Individual's icp.
+     * @return List of DocumentDto.
+     */
+    @Override
+    public List<DocumentDto> getArchiveDocuments(String icp) {
+        return getAllDocuments(icp).stream().filter(doc -> !doc.isActual()).toList();
+    }
+
+    /**
+     * Method to get an individual document by icp and document type.
+     *
+     * @param icp          - Individual's icp.
+     * @param documentType - Document type.
+     * @return DocumentDto.
+     * @throws DocumentsNotFoundException if the document with
+     *                                    the specified icp and document type is not found.
+     */
+    @Override
+    public DocumentDto getDocument(String icp, DocumentType documentType) {
+        return getAllDocuments(icp).stream()
+                .filter(Document -> Document.getDocumentType()
+                        .equals(documentType)).findFirst()
+                .orElseThrow(() -> new DocumentsNotFoundException(
+                        "Documents with icp " + icp + ", documentType "
+                                + documentType + " not found"));
+    }
 }
---
+++ src/test/java/org/kata/controller/DocumentControllerTest.java
@@ -0,0 +1,120 @@
+package org.kata.controller;
+
+import com.github.javafaker.Color;
+import lombok.Getter;
+import lombok.Setter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+
+import org.junit.Test;
+import org.kata.config.UrlProperties;
+import org.kata.dto.DocumentDto;
+import org.kata.dto.enums.DocumentType;
+import org.kata.exception.DocumentsNotFoundException;
+import org.kata.service.impl.DocumentServiceImpl;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Slf4j
+public class DocumentControllerTest {
+
+
+    @Test
+    public void getActualDocumentsTest() {
+        final UrlProperties urlProperties = new UrlProperties();
+        urlProperties.setProfileServiceBaseUrl("http://localhost:8081/");
+        urlProperties.setProfileServiceGetAllDocuments("v1/document/getAll");
+
+
+        DocumentController documentController = new DocumentController(new DocumentServiceImpl(urlProperties));
+        ResponseEntity<List<DocumentDto>> response = documentController.getActualDocuments("203-29-3983");
+        List<DocumentDto> documents = response.getBody();
+
+        log.info("Проверка на то что документ не равен null");
+        Assert.assertNotNull(documents.stream().findFirst().orElse(null));
+
+        log.info("Проверка на то что актуальный документ не равен неактуальному документу");
+        Assert.assertNotEquals(documents.stream().findFirst().orElse(null)
+                , documentController.getArchiveDocuments("203-29-3983").getBody()
+                        .stream().findFirst().orElse(null));
+
+        log.info("Проверка на вызов ошибки DocumentsNotFoundException");
+        Assert.assertThrows(DocumentsNotFoundException.class, () -> {
+            documentController.getActualDocuments("203-29-398");
+        });
+    }
+
+
+    @Test
+    public void getNotActualDocuments() {
+        final UrlProperties urlProperties = new UrlProperties();
+        urlProperties.setProfileServiceBaseUrl("http://localhost:8081/");
+        urlProperties.setProfileServiceGetAllDocuments("v1/document/getAll");
+
+        DocumentController documentController = new DocumentController(new DocumentServiceImpl(urlProperties));
+
+        log.info("Проверка на вызов ошибки DocumentsNotFoundException");
+        Assert.assertThrows(DocumentsNotFoundException.class, () -> {
+            documentController.getArchiveDocuments("203-29-398");
+        });
+
+        log.info("Проверка на то что документ не равен null");
+        Assert.assertNotNull(
+                documentController.getArchiveDocuments("203-29-3983")
+        );
+
+
+    }
+
+    @Test
+    public void getAllDocuments() {
+        final UrlProperties urlProperties = new UrlProperties();
+        urlProperties.setProfileServiceBaseUrl("http://localhost:8081/");
+        urlProperties.setProfileServiceGetAllDocuments("v1/document/getAll");
+
+        DocumentController documentController = new DocumentController(new DocumentServiceImpl(urlProperties));
+
+        log.info("Проверка на то что документ не равен null");
+
+        Assert.assertNotNull(
+                documentController.getAllDocuments("203-29-3983")
+        );
+
+        log.info("Проверка на вызов ошибки DocumentsNotFoundException");
+        Assert.assertThrows(DocumentsNotFoundException.class, () -> {
+            documentController.getAllDocuments("203-29-398");
+        });
+    }
+
+    @Test
+    public void getDocument() {
+        final UrlProperties urlProperties = new UrlProperties();
+        urlProperties.setProfileServiceBaseUrl("http://localhost:8081/");
+        urlProperties.setProfileServiceGetAllDocuments("v1/document/getAll");
+
+        DocumentController documentController = new DocumentController(new DocumentServiceImpl(urlProperties));
+
+
+        log.info("Проверка на то что документ не равен null");
+        Assert.assertNotNull(
+                documentController.getDocument("203-29-3983",
+                        DocumentType.valueOf("RF_DRIVING_LICENSE"))
+        );
+        log.info("Проверка на то что номер документа равен 5555555 ");
+        Assert.assertEquals("55555555",
+                documentController.getDocument("203-29-3983",
+                                DocumentType.valueOf("FRGN_PASSPORT"))
+                        .getBody().getDocumentNumber()
+        );
+
+        log.info("Проверка на вызов ошибки DocumentsNotFoundException");
+        Assert.assertThrows(DocumentsNotFoundException.class, () -> {
+            documentController.getDocument("203-29-398",
+                    DocumentType.valueOf("SNILS"));
+        });
+    }
+}
