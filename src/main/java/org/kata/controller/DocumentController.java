5874335f9bafd84e124dcc351569006cd1c6d43d
Камиль Хакимов <105122311+kamil116rus@users.noreply.github.com>
2023-11-06 11:06:06 +0300

    task-32 commit 2

--- src/main/java/org/kata/controller/DocumentController.java
+++ src/main/java/org/kata/controller/DocumentController.java
@@ -3,6 +3,8 @@ package org.kata.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Tag(name = "Documents", description = "This class is designed to work with Individual documents")
 @RestController
