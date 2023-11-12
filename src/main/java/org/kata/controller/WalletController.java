c50465b32f182aac95046aedbfd63df61fb1546e
kamil <kh8@ramler.ru>
2023-11-13 00:33:16 +0300

    добавил методы в контроллер, сервис. добавил доп.классы внес дополнения в пропертис

--- src/main/java/org/kata/controller/WalletController.java
+++ src/main/java/org/kata/controller/WalletController.java
@@ -3,6 +3,11 @@ package org.kata.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.kata.dto.WalletDto;
+import org.kata.service.WalletService;
+import org.kata.service.impl.WalletServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +20,15 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("v1/wallets")
 public class WalletController {
+  private  final WalletService walletService;
+
+
 
     @Operation(summary = "Получить Wallet по icp",
             description= "Возвращает DTO Wallet по ICP")
     @GetMapping
-    public ResponseEntity<List<WalletDto>> getDocument(String icp) {
-
-        return null;
+    public ResponseEntity<List<WalletDto>> getWallet(String icp) {
+        return new ResponseEntity<>(walletService.getWallets(icp), HttpStatus.OK);
     }
 
 }
