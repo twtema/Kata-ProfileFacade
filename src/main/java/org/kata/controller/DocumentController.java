2a1263dc5fd058f2b4be4be0a827e15d84160462
kamil <kh8@ramler.ru>
2023-11-13 00:34:42 +0300

    убрал лишние импорты

--- src/main/java/org/kata/controller/DocumentController.java
+++ src/main/java/org/kata/controller/DocumentController.java
@@ -3,8 +3,6 @@ package org.kata.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
