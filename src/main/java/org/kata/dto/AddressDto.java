7561b53803c177141dec5189157d267dfb0b9ad4
kamil <kh8@ramler.ru>
2023-11-12 23:12:22 +0300

    ДОБАВИЛ  ДТО и ЕНАМЫ

---
+++ src/main/java/org/kata/dto/AddressDto.java
@@ -0,0 +1,32 @@
+package org.kata.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder
+@Jacksonized
+@Schema(description = "DTO представляющий Адрес")
+public class AddressDto {
+
+    @Schema(description = "ICP", example = "1234567890")
+    private String icp;
+
+    @Schema(description = "Улица", example = "Пушкина")
+    private String street;
+
+    @Schema(description = "Город", example = "Москва")
+    private String city;
+
+    @Schema(description = "Государство", example = "Российская Федерация")
+    private String state;
+
+    @Schema(description = "Почтовый индекс", example = "101000")
+    private String postCode;
+
+    @Schema(description = "Страна", example = "Россия")
+    private String country;
+
+}
---
+++ src/main/java/org/kata/dto/enums/CurrencyType.java
@@ -0,0 +1,8 @@
+package org.kata.dto.enums;
+
+public enum CurrencyType {
+    RUB,
+    BYN,
+    USD,
+    CNY
+}
