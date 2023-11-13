627d3256ae29516ffcd6446f27aaee2d1f7b50f6
Aleksey Mischanchuk <91007076+groovypong@users.noreply.github.com>
2023-11-13 07:31:47 +0300

    Task 16: В ProfileFacade вывести методы контроллера для каждого типа документа (#7)
    Добавлены 5 контроллеров для вывода каждого документа

--- src/main/java/org/kata/controller/DocumentController.java
+++ src/main/java/org/kata/controller/DocumentController.java
@@ -3,8 +3,6 @@ package org.kata.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Tag(name = "Documents", description = "This class is designed to work with Individual documents")
 @RestController
@@ -73,6 +70,91 @@ public class DocumentController {
             (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp,
              @Parameter(description = "Document type", required = true) @RequestParam DocumentType documentType) {
         return new ResponseEntity<>(documentService.getDocument(icp, documentType), HttpStatus.OK);
+    }
+
+    /**
+     * Данный контроллер по icp сущности Individual возвращает его <b>RF Passport</b>
+     * @param icp Individual's ICP
+     * @return Individual's RF Passport in {@link DocumentDto} format
+     */
+    @Operation(summary = "Get Individual's RF Passport")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of document"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/getDocument/RFPassport")
+    public ResponseEntity<DocumentDto> getRFPassport
+            (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp) {
+        return new ResponseEntity<>(documentService.getDocument(icp, DocumentType.RF_PASSPORT), HttpStatus.OK);
+    }
+
+    /**
+     * Данный контроллер по icp сущности Individual возвращает его <b>FRGN Passport</b>
+     * @param icp Individual's ICP
+     * @return Individual's FRGN Passport in {@link DocumentDto} format
+     */
+    @Operation(summary = "Get Individual's Foreign Passport")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of document"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/getDocument/FRGNPassport")
+    public ResponseEntity<DocumentDto> getFRGNPASSPORT
+            (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp) {
+        return new ResponseEntity<>(documentService.getDocument(icp, DocumentType.FRGN_PASSPORT), HttpStatus.OK);
+    }
+
+    /**
+     * Данный контроллер по icp сущности Individual возвращает его <b>INN</b>
+     * @param icp Individual's ICP
+     * @return Individual's INN in {@link DocumentDto} format
+     */
+    @Operation(summary = "Get Individual's INN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of document"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/getDocument/INN")
+    public ResponseEntity<DocumentDto> getINN
+            (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp) {
+        return new ResponseEntity<>(documentService.getDocument(icp, DocumentType.INN), HttpStatus.OK);
+    }
+
+    /**
+     * Данный контроллер по icp сущности Individual возвращает его <b>SNILS</b>
+     * @param icp Individual's ICP
+     * @return Individual's SNILS in {@link DocumentDto} format
+     */
+    @Operation(summary = "Get Individual's SNILS")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of document"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/getDocument/SNILS")
+    public ResponseEntity<DocumentDto> getSNILS
+            (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp) {
+        return new ResponseEntity<>(documentService.getDocument(icp, DocumentType.SNILS), HttpStatus.OK);
+    }
+
+    /**
+     * Данный контроллер по icp сущности Individual возвращает его <b>RF Driving License</b>
+     * @param icp Individual's ICP
+     * @return Individual's RF Driving License in {@link DocumentDto} format
+     */
+    @Operation(summary = "Get Individual's RF Driving License")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of document"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/getDocument/RFDrivingLicense")
+    public ResponseEntity<DocumentDto> getRFDrivingLicense
+            (@Parameter(description = "ICP identifier", required = true) @RequestParam String icp) {
+        return new ResponseEntity<>(documentService.getDocument(icp, DocumentType.RF_DRIVING_LICENSE), HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
