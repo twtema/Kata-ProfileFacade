ab890cf6fa07e17e0015701a7163f9e908f3b19f
masamune <emopanda1337@gmail.com>
2024-01-15 01:18:54 +0700

    imports

--- src/main/java/org/kata/ProfileFacadeApplication.java
+++ src/main/java/org/kata/ProfileFacadeApplication.java
@@ -1,15 +1,7 @@
 package org.kata;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.kata.config.UrlProperties;
-import org.kata.controller.AddressController;
-import org.kata.dto.AddressDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootApplication
 public class ProfileFacadeApplication {
