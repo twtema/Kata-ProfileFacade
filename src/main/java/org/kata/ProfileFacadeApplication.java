f086a439f3e588680a5452224b08a8e85063b973
Bogdan <bog.bulgakov@yandex.ru>
2024-03-10 14:44:26 +0300

    Task 63 completed

--- src/main/java/org/kata/ProfileFacadeApplication.java
+++ src/main/java/org/kata/ProfileFacadeApplication.java
@@ -1,17 +1,11 @@
 package org.kata;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.kata.config.UrlProperties;
-import org.kata.controller.AddressController;
-import org.kata.dto.AddressDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.IOException;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProfileFacadeApplication {
 
     public static void main(String[] args) {
